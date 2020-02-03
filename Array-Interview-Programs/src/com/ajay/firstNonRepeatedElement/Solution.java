fb66d3d78487abfe76dd2f0731e1c78e517096c7
Ajinkya <patil28ajinkya@gmail.com>
2020-02-03 19:28:17 +0530

    First non repeated element in array

---
+++ Array-Interview-Programs/src/com/ajay/firstNonRepeatedElement/Solution.java
@@ -0,0 +1,36 @@
+package com.ajay.firstNonRepeatedElement;
+
+/*
+ *  i/p {1,2,3,2,3,1,5}
+ 	o/p {5}
+ */
+public class Solution {
+	public static void main(String[] args) {
+		int[] arr= {1,2,3,2,3,5};
+		firstNonRepeatingElement(arr);
+	}
+
+	private static void firstNonRepeatingElement(int[] arr) {
+		
+		int i=0;
+		while(i<arr.length) {
+			int j=0;
+			while(j<arr.length) {
+				if(i==j) {
+					j++;
+				}
+				else if(arr[i] == arr[j]){
+					break;
+				}
+				else if(j == arr.length-1) {
+					
+					System.out.println(arr[i]);
+				}
+				j++;
+			}
+			i++;
+		}
+		
+			
+	}
+}
