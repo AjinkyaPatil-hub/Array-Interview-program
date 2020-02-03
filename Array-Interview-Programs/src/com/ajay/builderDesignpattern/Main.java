2285be41214294500dca702d37c345fea664777b
AjinkyaPatil-hub <52791315+AjinkyaPatil-hub@users.noreply.github.com>
2020-02-04 01:09:54 +0530

    Delete Main.java

--- Array-Interview-Programs/src/com/ajay/builderDesignpattern/Main.java
+++
@@ -1,9 +0,0 @@
-package com.ajay.builderDesignpattern;
-
-public class Main {
-	public static void main(String[] args) {
-		Person peron = new PersonBuilder().setId(101).build();
-		System.out.println("BuilderDesigPattern>>>  "+peron);
-	}
-
-}
