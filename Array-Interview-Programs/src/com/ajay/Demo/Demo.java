87ab872aff058b52af89973a15a3a001df066450
AjinkyaPatil-hub <52791315+AjinkyaPatil-hub@users.noreply.github.com>
2020-02-05 15:58:20 +0530

    Update Demo.java

--- Array-Interview-Programs/src/com/ajay/Demo/Demo.java
+++ Array-Interview-Programs/src/com/ajay/Demo/Demo.java
@@ -4,5 +4,24 @@ public class Demo {
 
 	public static void main(String[] args) {
 		//i cahanged in demo
+		
+		
+		
 	}
+	private static void findSecondLargestElement(List<Integer> list) {
+		
+		int largest =list.get(0);
+		int secondLargest = list.get(0);
+		
+		for(int i=0;i<list.size();i++) {
+			if(list.get(i) > largest) {
+				secondLargest = largest;
+				largest = list.get(i);
+			}
+		}
+		System.out.println("second largest:" + secondLargest);
+		
+		
+	}
+
 }
